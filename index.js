59a80878f688c0a497b8839fd5f3df8037468c50
lizwachira <lizwanguwachira@gmail.com>
2024-03-21 01:53:30 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+//declare my variable
+let companyName; 
+companyName = 'Scuber'; 
+
+//const companyName = 'Scuber';  SyntaxError: Identifier 'companyName' has already been declared
+
+//Trying to reassign the value from 'Scuber'
+companyName; 
+companyName = 'Moby'
+//reassigned a new value 'Moby'
+companyName;
+
+ // declaring new variable & assign a new value.
+ let mostProfitableNeighborhood = 'Ngong';
+ mostProfitableNeighborhood;
+
+//declaring a new variable & assign value.
+ const ceoName = 'Elizabeth_Wang';
+ ceoName;
+
+//string concatenation using +
+console.log(companyName+ ' ' + mostProfitableNeighborhood+ ' ' + ceoName);
+
+// string interpolation
+console.log(`${companyName} ${mostProfitableNeighborhood} ${ceoName}`);
